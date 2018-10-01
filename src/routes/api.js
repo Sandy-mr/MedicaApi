49ef9c9040d184dd2ca3e6b49552634ba3ce0f85
Sandy-mr <sandy.mulato.r@gmail.com>
2018-10-01 12:39:25 -0500

    Api_Project

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,17 +6,21 @@ const app = Router();
 
 const HistorialPresion = require('../controllers/HistorialPresion');
 const InfoMedica = require('../controllers/InfoMedica');
+const User = require('../controllers/User');
 
-app.get('/HistorialPresion', HistorialPresion.index);
-app.post('/HistorialPresion', HistorialPresion.create);
+app.get(HistorialPresion.index);
+app.post('/isAuthenticated', HistorialPresion.create);
 // .put(HistorialPresion.update);
 
 app.delete('/HistorialPresion/:HistorialId', HistorialPresion.remove);
-// app.get('/HistorialPresion/:HistorialId', HistorialPresion.getById);
 
-app.get('/InfoMedica', InfoMedica.index);
-app.post('/InfoMedica', InfoMedica.create);
+app.get(InfoMedica.index);
+app.post('/isAuthenticated', InfoMedica.create);
 app.delete('/infoMedica/:InfoMedicaId', InfoMedica.remove);
 
+app.post('/auth/signup', User.create);
+app.post('/auth/login', User.login);
+app.get('/users', User.index);
+app.delete('/users/:userId', User.remove)
 
 module.exports = app;
