3c90730b030a835cb835e16091bd32fa857c0807
Sandy-mr <sandy.mulato.r@gmail.com>
2018-10-03 22:30:49 -0500

    add user name

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,134 +1,118 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 const Controller = {
-    index: (request, response) => {
-        User
-            .find({})
-            .exec()
-            .then(users => {
-                response
-                    .status(200)
-                    .json({
-                        users,
-                        total: users.length
-                    });
+  index: (request, response) => {
+    User.find({})
+      .exec()
+      .then(users => {
+        response.status(200).json({
+          users,
+          total: users.length
+        });
+      })
+      .catch(error => {
+        response.status(500).json({
+          error
+        });
+      });
+  },
+  create: (request, response) => {
+    User.find({
+      email: request.body.email
+    })
+      .exec()
+      .then(users => {
+        if (users.length < 1) {
+          const hash = bcrypt.hashSync(request.body.password);
+
+          const newUser = new User({
+            _id: mongoose.Types.ObjectId(),
+            nombre: request.body.nombre,
+            email: request.body.email,
+            password: hash
+          });
+
+          newUser
+            .save()
+            .then(saved => {
+              response.status(201).json({
+                message: "User created successfully."
+              });
             })
             .catch(error => {
-                response
-                    .status(500)
-                    .json({
-                        error
-                    });
+              response.status(500).json({
+                error
+              });
             });
-    },
-    create: (request, response) => {
-        User
-            .find({
-                email: request.body.email
-            })
-            .exec()
-            .then(users => {
-                if (users.length < 1) {
-                    const hash = bcrypt.hashSync(request.body.password);
+        } else {
+          response.status(422).json({
+            message: "User already exists."
+          });
+        }
+      })
+      .catch(error => console.log(error));
+  },
+  remove: (request, response) => {
+    User.findByIdAndRemove(request.params.userId)
+      .exec()
+      .then(() => {
+        response.status(200).json({
+          message: "User was deleted."
+        });
+      });
+  },
 
-                    const newUser = new User({
-                        _id: mongoose.Types.ObjectId(),
-                        email: request.body.email,
-                        password: hash
-                    });
+  login: (request, response) => {
+    User.find({
+      email: request.body.email
+    })
+      .exec()
+      .then(user => {
+        if (user.length > 0) {
+          bcrypt.compare(
+            request.body.password,
+            user[0].password,
+            (error, result) => {
+              if (error) {
+                return response.status(401).json({
+                  message: "Authentication failed."
+                });
+              }
 
-                    newUser
-                        .save()
-                        .then(saved => {
-                            response
-                                .status(201)
-                                .json({
-                                    message: 'User created successfully.'
-                                });
-                        })
-                        .catch(error => {
-                            response
-                                .status(500)
-                                .json({
-                                    error
-                                })
-                        });
-                } else {
-                    response
-                        .status(422)
-                        .json({
-                            message: 'User already exists.'
-                        });
-                }
-            })
-            .catch(error => console.log(error));
-    },
-    remove: (request, response) => {
-        User
-            .findByIdAndRemove(request.params.userId)
-            .exec()
-            .then(() => {
-                response
-                    .status(200)
-                    .json({
-                        message: 'User was deleted.'
-                    });
-            });
-    },
+              if (result) {
+                const token = jwt.sign(
+                  {
+                    email: user[0].email,
+                    userId: user[0]._id
+                  },
+                  process.env.JWT_SECRETKEY,
+                  {
+                    expiresIn: "1h"
+                  }
+                );
 
-    login: (request, response) => {
-        User
-            .find({
-                email: request.body.email
-            })
-            .exec()
-            .then(user => {
-                if (user.length > 0) {
+                return response.status(200).json({
+                  message: "Authentication successfull.",
+                  token
+                });
+              }
 
-                    bcrypt.compare(request.body.password, user[0].password, (error, result) => {
-                        if (error) {
-                            return response
-                                .status(401)
-                                .json({
-                                    message: 'Authentication failed.'
-                                })
-                        }
-
-                        if (result) {
-                            const token = jwt.sign({
-                                email: user[0].email,
-                                userId: user[0]._id
-                            }, process.env.JWT_SECRETKEY, {
-                                expiresIn: '1h'
-                            });
-
-                            return response
-                                .status(200)
-                                .json({
-                                    message: 'Authentication successfull.',
-                                    token
-                                });
-                        }
-
-                        response
-                            .status(401)
-                            .json({
-                                message: 'Authentication failed.'
-                            })
-                    });
-                } else {
-                    response
-                        .status(422)
-                        .json({
-                            message: 'Authentication failed.'
-                        })
-                }
-            });
-    }
+              response.status(401).json({
+                message: "Authentication failed."
+              });
+            }
+          );
+        } else {
+          response.status(422).json({
+            message: "Authentication failed."
+          });
+        }
+      });
+  }
 };
 
 module.exports = Controller;
--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  _id: mongoose.Schema.Types.ObjectId,
+  nombre: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('User', Schema);
+module.exports = mongoose.model("User", Schema);
